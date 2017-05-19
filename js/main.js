7c470e86d3fc5863cbcf0dfd2031e9d0998170f7
Lilulag <lilula739@gmail.com>
2017-05-19 17:18:17 +0300

    some of mistakes have fixed

--- js/main.js
+++ js/main.js
@@ -18,7 +18,45 @@ $(document).ready(function(){
 			}
 	})
 
+	$('.ourmenu__link').on('click', function(e){
 
+		e.preventDefault();
+
+		if ($(this).hasClass('active'))
+			{
+				$(this).siblings('.ourmenu__fulldesc').css('display','none');//slideDown(200);
+				$(this).removeClass('active');
+			}
+		else
+			{
+				var prevActive = $('.ourmenu__link.active');
+				prevActive.siblings('.ourmenu__fulldesc').slideUp(200);
+				prevActive.removeClass('active');
+
+				$(this).siblings('.ourmenu__fulldesc').slideDown(200).css('display','inline-block'); 
+				$(this).addClass('active');
+			}
+	})
+
+
+	// $('.formorder__itemtext-shift').on('click', function(e){
+
+	// 	if ($(this).hasClass('active'))
+	// 		{
+	// 			#f9b43b
+	// 			$(this:after).siblings('.ourmenu__fulldesc').css('display','none');//slideDown(200);
+	// 			$(this).removeClass('active');
+	// 		}
+	// 	else
+	// 		{
+	// 			var prevActive = $('.ourmenu__link.active');
+	// 			prevActive.siblings('.ourmenu__fulldesc').slideUp(200);
+	// 			prevActive.removeClass('active');
+
+	// 			$(this).siblings('.ourmenu__fulldesc').slideDown(200).css('display','inline-block'); 
+	// 			$(this).addClass('active');
+	// 		}
+	// })
 
 })
 
